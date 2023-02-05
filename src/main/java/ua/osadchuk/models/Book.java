96f538075c4c383f97be46afe5906ad8c7cd2c42
Taras Osadchuk <tarasosadchuk.v@gmail.com>
2023-02-05 14:09:01 +0200

    Initial commit

---
+++ src/main/java/ua/osadchuk/models/Book.java
@@ -0,0 +1,64 @@
+package ua.osadchuk.models;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Book {
+    private int id;
+
+    @NotEmpty(message = "Book title must not be empty")
+    @Size(min = 2, max = 100, message = "Book title must be between 2 and 100 characters long")
+    private String title;
+
+    @NotEmpty(message = "Author must not be empty")
+    @Size(min = 2, max = 100, message = "Author name must be between 2 and 100 characters long")
+    private String author;
+
+    @Min(value = 1500, message = "Year must be greater than 1500")
+    @Max(value = 2023, message = "The year must be less than 2023")
+    private int year;
+
+    public Book() {
+
+    }
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ src/main/java/ua/osadchuk/models/Person.java
@@ -0,0 +1,51 @@
+package ua.osadchuk.models;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Name must not be empty")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters long")
+    private String fullName;
+
+    @Min(value = 1900, message = "Year of birth must be greater than 1900")
+    @Max(value = 2023, message = "The year must be less than 2023")
+    private int yearOfBirth;
+
+    public Person() {
+
+    }
+
+    public Person(String fullName, int yearOfBirth) {
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
